37c12403082d25e510378bb4defecd97e683ddcf
cm42git <106208621+cm42git@users.noreply.github.com>
2022-10-18 13:58:13 -0400

    springdoc swagger with auth

--- capstone/java/src/main/java/com/techelevator/ApiDocConfig/DocConfig.java
+++ capstone/java/src/main/java/com/techelevator/ApiDocConfig/DocConfig.java
@@ -1,4 +1,4 @@
-package com.techelevator.ApiDocConfig;
+/* package com.techelevator.ApiDocConfig;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -52,4 +52,51 @@ public class DocConfig {
                 .paths(PathSelectors.any())
                 .build();
     }
+}
+ */
+package com.techelevator.ApiDocConfig;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.util.StringUtils;
+
+@Configuration
+public class DocConfig {
+
+        private final String moduleName = "Project Schedule App";
+        private final String apiVersion = "0.1";
+
+        /*
+         * public DocConfig(
+         * 
+         * @Value("${module-name}") String moduleName,
+         * 
+         * @Value("${api-version}") String apiVersion) {
+         * this.moduleName = moduleName;
+         * this.apiVersion = apiVersion;
+         * }
+         */
+
+        @Bean
+        public OpenAPI customOpenAPI() {
+                final String securitySchemeName = "bearerAuth";
+                final String apiTitle = String.format("%s API", StringUtils.capitalize(moduleName));
+                return new OpenAPI()
+                                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                                .components(
+                                                new Components()
+                                                                .addSecuritySchemes(securitySchemeName,
+                                                                                new SecurityScheme()
+                                                                                                .name(securitySchemeName)
+                                                                                                .type(SecurityScheme.Type.HTTP)
+                                                                                                .scheme("bearer")
+                                                                                                .bearerFormat("JWT")
+                                                                                                .description("Enter JWT")))
+                                .info(new Info().title(apiTitle).version(apiVersion));
+        }
 }
